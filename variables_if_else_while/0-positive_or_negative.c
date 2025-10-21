2d296a23e6cb809ec35f87319caa7c54853a2292
Khai <12096@holbertonstudents.com>
2025-10-21 11:49:48 +0000

    Added header

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
 /**
  * main - entry point
@@ -19,7 +19,7 @@ int main(void)
     } else if (n == 0) {
         printf("%d is zero\n", n);
     } else {
-        printf("%d is negative\n", n):
+        printf("%d is negative\n", n);
     }
 	return (0);
 }
