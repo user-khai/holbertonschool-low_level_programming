e28c25f5fe06cf4dfc913119529eb2d9efe01237
Khai <12096@holbertonstudents.com>
2025-10-22 14:37:57 +0000

    Deleted trailing whitespace

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -18,7 +18,7 @@ void times_table(void)
 		while (b < 10)
 		{
 			c = a * b;
-			
+
 			if (b == 0)
 			{
 				_putchar(c + '0');
@@ -27,7 +27,7 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-			
+
 				if (c < 10)
 				{
 					_putchar(' ');
