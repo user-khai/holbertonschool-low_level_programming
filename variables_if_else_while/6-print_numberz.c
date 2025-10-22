fc5f12d91e62e0e6ea9d0c77996d7a42c6403bfa
Khai <12096@holbertonstudents.com>
2025-10-22 06:37:26 +0000

    Add 6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: prints integers 0-9
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
