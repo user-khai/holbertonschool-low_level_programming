315e3f7b24419ff8877e2a3bb22a40ecbca751fe
Khai <12096@holbertonstudents.com>
2025-10-22 07:51:17 +0000

    Add 0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar.h"
+#include <unistd.h>
+
+/**
+ * main - entry point
+ * Description: Prints out _putchar using the function _putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return(0);
+}
