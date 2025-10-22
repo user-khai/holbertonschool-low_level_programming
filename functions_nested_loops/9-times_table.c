8029ee7ebdfc5c59364606cd65cc6375c30a7386
Khai <12096@holbertonstudents.com>
2025-10-22 13:23:06 +0000

    Fixed filename

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - print up until x9 times table
+ */
+void times_table(void)
+{
+	int a = 0;
+	int b = 0;
+	int c;
+
+	while (a < 10)
+	{
+		if (b > 9)
+		{
+			b = 0;
+		}
+		while (b < 10)
+		{
+			c = a * b;
+			_putchar(c + '0');
+			if (b < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				if (c < 10)
+				{
+					_putchar(' ');
+				}
+			}
+			b++;
+		}
+		a++;
+	}
+}
