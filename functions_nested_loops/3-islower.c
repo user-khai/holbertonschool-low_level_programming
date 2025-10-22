8ef32ac21cba33b9c075c2f2bde4e4d52c9e647b
Khai <12096@holbertonstudents.com>
2025-10-22 11:04:19 +0000

    Add 3-islower.c

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: On success 1.
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
