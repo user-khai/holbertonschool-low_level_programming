41a886e8716046490abc61d6adeae13cc77ddc7a
Khai <12096@holbertonstudents.com>
2025-10-22 13:52:59 +0000

    Fixed formatting

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -19,7 +19,15 @@ void times_table(void)
 		while (b < 10)
 		{
 			c = a * b;
-			_putchar(c + '0');
+			
+			if (c < 10)
+			{
+				_putchar(c + '0');
+			}
+			else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			if (b < 9)
 			{
 				_putchar(',');
