e9ad1a8ee966a91de9c0af8d235793e00cb76017
Khai <12096@holbertonstudents.com>
2025-10-22 06:31:21 +0000

    Add 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: prints integers 0-9
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	return (0);
+}
