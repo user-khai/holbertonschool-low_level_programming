708b2e0a79ebed698246449799f6402bfdf51cf2
Khai <12096@holbertonstudents.com>
2025-10-22 15:00:27 +0000

    Added description

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * print_to_98 - prints numbers ranging from n-98
+ * @n: the start of the range
+ * Description: numbers are separated by a comma and a space
+ */
 void print_to_98(int n)
 {
 	if (n <= 98)
