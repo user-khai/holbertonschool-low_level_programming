ad5b49834f25ca2e9626b4070d7329387a337be2
Khai <12096@holbertonstudents.com>
2025-10-22 11:33:02 +0000

    Add 6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value of a number
+ * @n: the number the return the absolute value of
+ * Return: |n|
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	return (n);
+}
