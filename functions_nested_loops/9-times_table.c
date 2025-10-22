0b892d8267fba5dbce2d45633e6e7fa427f314e9
Khai <12096@holbertonstudents.com>
2025-10-22 14:29:30 +0000

    Fixed conditions

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -23,20 +23,20 @@ void times_table(void)
 			{
 				_putchar(c + '0');
 			}
-			if (b > 0 && b < 10)
-			{
-				_putchar(' ');
-				_putchar(c + '0');
-			}
 			else
-			{
-				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
-			}
-			if (b < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
+			
+				if (c < 10)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((c / 10) + '0');
+				}
+				_putchar((c % 10) + '0');
 			}
 			b++;
 		}
