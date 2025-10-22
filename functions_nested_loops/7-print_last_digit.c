2f584e8890ca38c51e8c02f260ba813f6e22512b
Khai <12096@holbertonstudents.com>
2025-10-22 11:50:45 +0000

    Added _putchar

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -7,9 +7,15 @@
  */
 int print_last_digit(int n)
 {
+	int last_digit;
+
 	if (n < 0)
 	{
-		return ((n % 10) * -1);
+		last_digit = (n % 10) * -1;
+		_putchar(last_digit);
+		return (last_digit);
 	}
-	return (n % 10);
+	last_digit = n % 10;
+	_putchar(last_digit);
+	return (last_digit);
 }
