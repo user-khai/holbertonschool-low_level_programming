78611fcfab031de3bf929930b99f182b47875560
Khai <12096@holbertonstudents.com>
2025-10-22 12:46:55 +0000

    Add 8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints time starting from 0:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int hour = 0;
+	int minute = 0;
+
+	while (hour < 24)
+	{
+		while (minute < 60)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
