1804b3d5b3974a27ab1d77592b316e4b90f14353
Khai <12096@holbertonstudents.com>
2025-10-22 11:54:01 +0000

    Fixed up _putchar arguments

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,8 @@ int print_last_digit(int n)
 	if (n < 0)
 	{
 		last_digit = (n % 10) * -1;
-		_putchar(last_digit);
-		return (last_digit);
 	}
 	last_digit = n % 10;
-	_putchar(last_digit);
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
