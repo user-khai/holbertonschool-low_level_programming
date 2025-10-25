88424ef439069edb68687f9548ca4f6d7f13ab41
Khai <12096@holbertonstudents.com>
2025-10-25 13:48:31 +0000

    Add space

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,21 +13,25 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
 		i++;
+		if (i != 101)
+		{
+			printf(" ");
+		}
 	}
 	return (0);
 }
