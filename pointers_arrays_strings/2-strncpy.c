a6852298ef3f78293df9b53a4a81ed6e0176df56
Khai <12096@holbertonstudents.com>
2025-10-27 14:28:01 +0000

    Added while loop for when src is shorter than n

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,10 +11,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (src[i] != '\0' && i < n && src[i])
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
