90b841a58da44b98b43f46495ca2247eaae23839
Khai <12096@holbertonstudents.com>
2025-10-27 14:10:27 +0000

    Add 2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies n bytes from src to dest
+ * @dest: string to be concatenated to
+ * @src: string
+ * @n: number of bytes to use from src
+ * Return: pointer to the resulting string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
