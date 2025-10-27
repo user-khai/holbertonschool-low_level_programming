28835fa8a879b74b3e2a1d40872dd7c8533d7d59
Khai <12096@holbertonstudents.com>
2025-10-27 08:31:27 +0000

    Add 6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: the string
+ * Description: starts from the first character and prints a newline at the end
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while(str[i] != '\0' && str)
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+	_putchar('\n');
+}
