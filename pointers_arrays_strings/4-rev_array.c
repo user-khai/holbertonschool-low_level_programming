1c3e81a6fe34e5612c99c33d4788ffd701963d4d
Khai <12096@holbertonstudents.com>
2025-10-28 01:03:35 +0000

    Add 4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an int array
+ * @a: array
+ * @n: number of elements in array
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0, tmp;
+
+	n -= 1;
+	while (i < n / 2)
+	{
+		tmp = a[i];
+		a[i] = a[n - i];
+	        a[n - i] = tmp;
+	}
+}	
