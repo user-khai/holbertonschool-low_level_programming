9711ea76de8994ab18dddd9d23fb270e0971d87e
Khai <12096@holbertonstudents.com>
2025-11-02 11:24:11 +0000

    Added space before parenthesis

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -11,7 +11,7 @@ void print_diagsums(int *a, int size)
 {
 	int i = 0, sum1 = 0, sum2 = 0;
 
-	while(i < size)
+	while (i < size)
 	{
 		sum1 += a[i * size + i];
 		sum2 += a[i * size + (size - i - 1)];
