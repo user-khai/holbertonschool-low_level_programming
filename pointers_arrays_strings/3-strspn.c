9b4477c9e1ecf9d77175a87792557be18eccb209
Khai <12096@holbertonstudents.com>
2025-11-02 07:28:46 +0000

    Add 3-strspn.c

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * _strscpn - gets the length of a prefix substring
+ * @s: char pointer to string
+ * @accept: char pointer to string
+ * Return: number of bytes in s found in accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int found;
+
+	while (*s != '\0')
+	{
+		char *a_ptr = accept;
+		found = 0;
+
+		while (*a_ptr != '\0')
+		{
+			if (*s == *a_ptr)
+			{
+				found = 1;
+				break;
+			}
+			a_ptr++;
+		}
+		if (found)
+		{
+			count++;
+			s++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return count;
+}
