d1d7e4ae2ef053c1b9457e93cb7e39e64bbd8697
Khai <12096@holbertonstudents.com>
2025-11-02 07:55:49 +0000

    Add 4-strpbrk.c

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locates the first occurrence of any bytes of accept in s
+ * @s: char pointer to string
+ * @accept: char pointer to string
+ * Return: pointer to the first instance in s that matches a char in accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int count = 0;
+
+	while (*s != '\0')
+	{
+		while (accept[count] != '\0')
+		{
+			if (*s == accept[count])
+			{
+				return(s);
+			}
+			count++;
+		}
+		count = 0;
+		s++;
+	}
+	return (NULL);
+}
