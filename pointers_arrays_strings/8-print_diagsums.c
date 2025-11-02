d5a2c3fa4abd3709b5a827e2a86d0fd7893a7aed
Khai <12096@holbertonstudents.com>
2025-11-02 11:22:42 +0000

    Fixed pointer arithmetic logic

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,17 +9,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i = 0, j = size - 1, sum1 = 0, sum2 = 0;
+	int i = 0, sum1 = 0, sum2 = 0;
 
 	while(i < size)
 	{
-		sum1 += a[i][i];
+		sum1 += a[i * size + i];
+		sum2 += a[i * size + (size - i - 1)]
 		i++;
-	}
-	while (j >= 0)
-	{
-		sum2 += a[j][j];
-		j--;
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
